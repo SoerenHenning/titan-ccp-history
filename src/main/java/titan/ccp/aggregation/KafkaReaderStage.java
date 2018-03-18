85f3d9e3880445de54ee439c71931d77b3c08e41
Sören Henning <post@soeren-henning.de>
2018-03-18 14:55:55 +0100

    added KafkaReaderStage

---
+++ src/main/java/titan/ccp/aggregation/KafkaReaderStage.java
@@ -0,0 +1,29 @@
+package titan.ccp.aggregation;
+
+import java.util.concurrent.CompletableFuture;
+
+import kieker.common.record.IMonitoringRecord;
+import teetime.framework.AbstractProducerStage;
+
+public class KafkaReaderStage extends AbstractProducerStage<IMonitoringRecord> {
+
+	private final KafkaReader kafkaReader;
+
+	public KafkaReaderStage() {
+		this.kafkaReader = new KafkaReader(this::sendRecord);
+	}
+
+	@Override
+	protected void execute() throws Exception {
+		this.kafkaReader.run();
+	}
+
+	public CompletableFuture<Void> requestTermination() {
+		return this.kafkaReader.requestTermination();
+	}
+
+	private void sendRecord(final IMonitoringRecord record) {
+		this.getOutputPort().send(record);
+	}
+
+}
