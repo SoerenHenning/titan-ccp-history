8e3d2f88c886d66cd907ce03fe2487f8bef6d67d
Sören Henning <post@soeren-henning.de>
2018-03-29 15:14:23 +0200

    added experimental Kafka Streams implementation

--- src/main/java/titan/ccp/aggregation/experimental/kafkastreams/KafkaStreamsTest.java
+++ src/main/java/titan/ccp/aggregation/experimental/kafkastreams/KafkaStreamsTest.java
@@ -27,16 +27,24 @@ public class KafkaStreamsTest {
 
 		final StreamsBuilder builder = new StreamsBuilder(); // when using the DSL
 
-		final KStream<String, Long> wordCounts = builder.stream("word-counts-input-topic", /* input topic */
+		final KStream<String, String> wordCounts = builder.stream("input-topic-2", /* input topic */
 				Consumed.with(Serdes.String(), /* key serde */
-						Serdes.Long() /* value serde */
+						Serdes.String() /* value serde */
 				));
-		final KGroupedStream<String, Long> groupedStream = wordCounts.groupByKey();
+		final KGroupedStream<String, String> groupedStream = wordCounts.groupByKey();
 
 		final KTable<String, Long> aggregated = groupedStream.aggregate(() -> null, /* initializer */
-				(aggKey, newValue, aggValue) -> newValue, /* adder */
-				Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as(
-						"aggregated-stream-store" /* state store name */).withKeySerde(Serdes.String()) /* key serde */
+				(aggKey, newValue, aggValue) -> {
+					try {
+						System.out.println(aggKey + ":" + newValue);
+						return Long.parseLong(newValue);
+					} catch (final NumberFormatException e) {
+						return -1l;
+					}
+				}, /* adder */
+				Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as("aggregated-stream-store-2") // state store
+																											// name
+						.withKeySerde(Serdes.String()) /* key serde */
 						.withValueSerde(Serdes.Long())); /* serde for aggregate value */
 
 		final Topology topology = builder.build();
@@ -47,7 +55,7 @@ public class KafkaStreamsTest {
 		// and so on.
 		final Properties settings = new Properties();
 		// Set a few key parameters
-		settings.put(StreamsConfig.APPLICATION_ID_CONFIG, "my-first-streams-application");
+		settings.put(StreamsConfig.APPLICATION_ID_CONFIG, "my-first-streams-application-0.0.1");
 		settings.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 		// Any further settings
 		// settings.put(... , ...);
@@ -57,6 +65,33 @@ public class KafkaStreamsTest {
 
 		// Start the Kafka Streams threads
 		streams.start();
+
+		// while (true) {
+		// try {
+		// System.out.println("----------");
+		// final ReadOnlyKeyValueStore<String, Long> store =
+		// streams.store("aggregated-stream-store-2",
+		// QueryableStoreTypes.<String, Long>keyValueStore());
+		// for (final KeyValue<String, Long> x : (Iterable<KeyValue<String, Long>>) ()
+		// -> store.all()) {
+		// System.out.println("key: " + x.key + " val: " + x.value);
+		// }
+		// System.out.println(".");
+		// final Collection<StreamsMetadata> allMetadataForStore = streams
+		// .allMetadataForStore("aggregated-stream-store-2");
+		// System.out.println(allMetadataForStore.toString());
+		// for (final StreamsMetadata streamsMetadata : allMetadataForStore) {
+		// System.out.println(streamsMetadata.toString());
+		// }
+		// try {
+		// Thread.sleep(5000);
+		// } catch (final InterruptedException e) {
+		// e.printStackTrace();
+		// }
+		// } catch (final InvalidStateStoreException e) {
+		// e.printStackTrace();
+		// }
+		// }
 
 	}
 
