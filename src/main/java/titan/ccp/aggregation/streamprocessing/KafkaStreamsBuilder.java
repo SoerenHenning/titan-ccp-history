03a8422a0197936866e98947deda57fa3b6995d9
Sören Henning <post@soeren-henning.de>
2018-09-11 14:03:58 +0200

    clean up

--- src/main/java/titan/ccp/aggregation/streamprocessing/KafkaStreamsBuilder.java
+++ src/main/java/titan/ccp/aggregation/streamprocessing/KafkaStreamsBuilder.java
@@ -24,6 +24,7 @@ import org.slf4j.LoggerFactory;
 
 import com.datastax.driver.core.Session;
 
+import kieker.common.record.IMonitoringRecord;
 import titan.ccp.common.kieker.cassandra.CassandraWriter;
 import titan.ccp.common.kieker.cassandra.ExplicitPrimaryKeySelectionStrategy;
 import titan.ccp.common.kieker.cassandra.PredefinedTableNameMappers;
@@ -86,99 +87,53 @@ public class KafkaStreamsBuilder {
 		final KStream<String, ActivePowerRecord> inputStream = builder.stream(this.inputTopic,
 				Consumed.with(Serdes.String(), IMonitoringRecordSerde.serde(new ActivePowerRecordFactory())));
 
-		inputStream.foreach((k, v) -> LOGGER.info("Received record {}.", v)); // TODO Temporary;
+		inputStream.foreach((k, v) -> LOGGER.info("Received record {}.", v)); // TODO Temporary
 
 		final KStream<String, ActivePowerRecord> flatMapped = inputStream.flatMap((key, value) -> this.flatMap(value));
 
 		final KGroupedStream<String, ActivePowerRecord> groupedStream = flatMapped.groupByKey(
 				Serialized.with(Serdes.String(), IMonitoringRecordSerde.serde(new ActivePowerRecordFactory())));
 
-		final KTable<String, AggregationHistory> aggregated = groupedStream.aggregate(() -> {
-			return new AggregationHistory();
-		}, (aggKey, newValue, aggValue2) -> {
-			// System.out.println(".");
-			// System.out.println("__");
-			// System.out.println("O: " + aggKey + ": " + aggValue2.getLastValues());
-			// System.out.println("O: " + aggKey + ": " + aggValue2.getSummaryStatistics());
-			// System.out.println("new: " + newValue.getIdentifier() + ": " +
-			// newValue.getPowerConsumptionInWh());
-			// System.out.println("New record in aggregation."); // TODO
-			aggValue2.update(newValue);
-			// System.out.println("N: " + aggKey + ": " + aggValue2.getLastValues());
-			// System.out.println("N: " + aggKey + ": " + aggValue2.getSummaryStatistics());
-			// System.out.println("P: " + aggValue2.getTimestamp());
-			LOGGER.info("update history {}", aggValue2); // TODO
-			return aggValue2;
-			// return aggValue2.update(newValue);
-		}, Materialized.<String, AggregationHistory, KeyValueStore<Bytes, byte[]>>as(this.aggregationStoreName)
-				.withKeySerde(Serdes.String()).withValueSerde(AggregationHistorySerde.serde()));
-
-		// aggregated.toStream().foreach((key, value) -> {
-		// System.out.println("A: " + value.getTimestamp());
-		// System.out.println("A: " + key + ": " + value.getSummaryStatistics());
-		// }); // TODO
+		final KTable<String, AggregationHistory> aggregated = groupedStream.aggregate(() -> new AggregationHistory(),
+				(aggKey, newValue, aggValue2) -> {
+					aggValue2.update(newValue);
+					LOGGER.info("update history {}", aggValue2); // TODO
+					return aggValue2;
+				}, Materialized.<String, AggregationHistory, KeyValueStore<Bytes, byte[]>>as(this.aggregationStoreName)
+						.withKeySerde(Serdes.String()).withValueSerde(AggregationHistorySerde.serde()));
 
 		aggregated.toStream().map((key, value) -> KeyValue.pair(key, value.toRecord(key))).to(this.outputTopic,
 				Produced.with(Serdes.String(), IMonitoringRecordSerde.serde(new AggregatedActivePowerRecordFactory())));
 
 		// Cassandra Writer for AggregatedActivePowerRecord
-
-		final CassandraWriter cassandraWriter = this.buildCassandraWriter();
+		final CassandraWriter cassandraWriter = this.buildCassandraWriter(AggregatedActivePowerRecord.class);
 		builder.stream(this.outputTopic,
 				Consumed.with(Serdes.String(), IMonitoringRecordSerde.serde(new AggregatedActivePowerRecordFactory())))
 				.foreach((key, record) -> {
 					LOGGER.info("write to cassandra {}", record);
-					cassandraWriter.write(record); // System.out.println("New written"); // TODO
+					cassandraWriter.write(record);
 				});
 
-		// End Cassandra Writer
-
 		// Cassandra Writer for ActivePowerRecord
-
-		final CassandraWriter cassandraWriterForNormal = this.buildCassandraWriterForNormal();
+		final CassandraWriter cassandraWriterForNormal = this.buildCassandraWriter(ActivePowerRecord.class);
 		inputStream.foreach((key, record) -> {
+			LOGGER.info("write to cassandra {}", record);
 			cassandraWriterForNormal.write(record);
-			// System.out.println("New written"); // TODO
 		});
-
-		// End Cassandra Writer
 
 		return builder.build();
 	}
 
-	private CassandraWriter buildCassandraWriter() {
+	private CassandraWriter buildCassandraWriter(final Class<? extends IMonitoringRecord> recordClass) {
 		final ExplicitPrimaryKeySelectionStrategy primaryKeySelectionStrategy = new ExplicitPrimaryKeySelectionStrategy();
-		primaryKeySelectionStrategy.registerPartitionKeys(AggregatedActivePowerRecord.class.getSimpleName(),
-				"identifier");
-		primaryKeySelectionStrategy.registerClusteringColumns(AggregatedActivePowerRecord.class.getSimpleName(),
-				"timestamp");
+		primaryKeySelectionStrategy.registerPartitionKeys(recordClass.getSimpleName(), "identifier");
+		primaryKeySelectionStrategy.registerClusteringColumns(recordClass.getSimpleName(), "timestamp");
 
 		final CassandraWriter cassandraWriter = CassandraWriter.builder(this.cassandraSession).excludeRecordType()
 				.excludeLoggingTimestamp().tableNameMapper(PredefinedTableNameMappers.SIMPLE_CLASS_NAME)
 				.primaryKeySelectionStrategy(primaryKeySelectionStrategy).build();
 
 		return cassandraWriter;
-	}
-
-	// BETTER refine name
-	private CassandraWriter buildCassandraWriterForNormal() {
-		final ExplicitPrimaryKeySelectionStrategy primaryKeySelectionStrategy = new ExplicitPrimaryKeySelectionStrategy();
-		primaryKeySelectionStrategy.registerPartitionKeys(ActivePowerRecord.class.getSimpleName(), "identifier");
-		primaryKeySelectionStrategy.registerClusteringColumns(ActivePowerRecord.class.getSimpleName(), "timestamp");
-
-		final CassandraWriter cassandraWriter = CassandraWriter.builder(this.cassandraSession).excludeRecordType()
-				.excludeLoggingTimestamp().tableNameMapper(PredefinedTableNameMappers.SIMPLE_CLASS_NAME)
-				.primaryKeySelectionStrategy(primaryKeySelectionStrategy).build();
-
-		return cassandraWriter;
-	}
-
-	private StreamsConfig buildStreamConfig() {
-		final Properties settings = new Properties();
-		settings.put(StreamsConfig.APPLICATION_ID_CONFIG, "titanccp-aggregation-0.0.7"); // TODO as parameter
-		settings.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 1000); // TODO as parameter
-		settings.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, this.bootstrapServers);
-		return new StreamsConfig(settings);
 	}
 
 	private Iterable<KeyValue<String, ActivePowerRecord>> flatMap(final ActivePowerRecord record) {
@@ -187,7 +142,15 @@ public class KafkaStreamsBuilder {
 				.getSensorForIdentifier(record.getIdentifier()).stream().flatMap(s -> s.getParents().stream())
 				.map(s -> s.getIdentifier()).map(i -> KeyValue.pair(i, record)).collect(Collectors.toList());
 		LOGGER.info("Flat map result: {}", result); // TODO Temporary
-		return result; // TODO Temporary
+		return result;
+	}
+
+	private StreamsConfig buildStreamConfig() {
+		final Properties settings = new Properties();
+		settings.put(StreamsConfig.APPLICATION_ID_CONFIG, "titanccp-aggregation-0.0.7"); // TODO as parameter
+		settings.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 1000); // TODO as parameter
+		settings.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, this.bootstrapServers);
+		return new StreamsConfig(settings);
 	}
 
 }
