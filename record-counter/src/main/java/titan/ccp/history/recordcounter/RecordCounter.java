ceccdf99995df3be12b70ebb1ab559948d77a9e2
Sören Henning <post@soeren-henning.de>
2018-12-07 10:58:31 +0100

    Update to changed API for Casssandra writer

--- record-counter/src/main/java/titan/ccp/history/recordcounter/RecordCounter.java
+++ record-counter/src/main/java/titan/ccp/history/recordcounter/RecordCounter.java
@@ -5,8 +5,8 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import redis.clients.jedis.Jedis;
-import titan.ccp.common.kieker.cassandra.SessionBuilder;
-import titan.ccp.common.kieker.cassandra.SessionBuilder.ClusterSession;
+import titan.ccp.common.cassandra.SessionBuilder;
+import titan.ccp.common.cassandra.SessionBuilder.ClusterSession;
 import titan.ccp.history.api.ActivePowerRepository;
 import titan.ccp.models.records.ActivePowerRecord;
 
