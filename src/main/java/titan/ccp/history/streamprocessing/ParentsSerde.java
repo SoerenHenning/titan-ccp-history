03ddc91d8ebef897e898d008213a8b1cd3d5553e
Sören Henning <post@soeren-henning.de>
2019-03-15 11:07:42 +0100

    Use SimpleSerde libarary

--- src/main/java/titan/ccp/history/streamprocessing/ParentsSerde.java
+++ src/main/java/titan/ccp/history/streamprocessing/ParentsSerde.java
@@ -1,103 +1,46 @@
 package titan.ccp.history.streamprocessing;
 
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
-import org.apache.kafka.common.serialization.ByteBufferDeserializer;
-import org.apache.kafka.common.serialization.ByteBufferSerializer;
-import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.Serde;
-import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.common.serialization.Serializer;
+import titan.ccp.common.kafka.simpleserdes.BufferDeserializer;
+import titan.ccp.common.kafka.simpleserdes.BufferSerializer;
+import titan.ccp.common.kafka.simpleserdes.ReadBuffer;
+import titan.ccp.common.kafka.simpleserdes.SimpleSerdes;
+import titan.ccp.common.kafka.simpleserdes.WriteBuffer;
 
 /**
  * {@link Serde} factory for {@link Set} of parent identifiers.
  */
-public final class ParentsSerde {
-
-  private static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
+public final class ParentsSerde
+    implements BufferSerializer<Set<String>>, BufferDeserializer<Set<String>> {
+  // public final class ParentsSerde implements BufferSerde<Set<String>> {
 
   private ParentsSerde() {}
 
+  @Override
+  public void serialize(final WriteBuffer buffer, final Set<String> parents) {
+    buffer.putInt(parents.size());
+    for (final String parent : parents) {
+      buffer.putString(parent);
+    }
+  }
+
+  @Override
+  public Set<String> deserialize(final ReadBuffer buffer) {
+    final int size = buffer.getInt();
+    final Set<String> parents = new HashSet<>(size);
+    for (int i = 0; i < size; i++) {
+      final String parent = buffer.getString();
+      parents.add(parent);
+    }
+    return parents;
+  }
+
   public static Serde<Set<String>> serde() {
-    return Serdes.serdeFrom(new ParentsSerializer(), new ParentsDeserializer());
-  }
-
-  private static class ParentsSerializer implements Serializer<Set<String>> {
-
-    private static final int BYTE_BUFFER_CAPACITY = 65536; // Is only virtual memory
-
-    private final ByteBufferSerializer byteBufferSerializer = new ByteBufferSerializer();
-
-    @Override
-    public void configure(final Map<String, ?> configs, final boolean isKey) {
-      this.byteBufferSerializer.configure(configs, isKey);
-    }
-
-    @Override
-    public byte[] serialize(final String topic, final Set<String> parents) {
-      if (parents == null) {
-        return null;
-      }
-
-      final ByteBuffer buffer = ByteBuffer.allocateDirect(BYTE_BUFFER_CAPACITY);
-
-      buffer.putInt(parents.size());
-      for (final String parent : parents) {
-        final byte[] bytes = parent.getBytes(DEFAULT_CHARSET);
-        buffer.putInt(bytes.length);
-        buffer.put(bytes);
-      }
-
-      buffer.flip();
-      return this.byteBufferSerializer.serialize(topic, buffer);
-    }
-
-    @Override
-    public void close() {
-      this.byteBufferSerializer.close();
-    }
-
-  }
-
-  private static class ParentsDeserializer implements Deserializer<Set<String>> {
-
-    private final ByteBufferDeserializer byteBufferDeserializer = new ByteBufferDeserializer();
-
-    @Override
-    public void configure(final Map<String, ?> configs, final boolean isKey) {
-      this.byteBufferDeserializer.configure(configs, isKey);
-    }
-
-    @Override
-    public Set<String> deserialize(final String topic, final byte[] data) {
-      final ByteBuffer buffer = this.byteBufferDeserializer.deserialize(topic, data);
-
-      if (buffer == null) {
-        // Verify null/empty set
-        return null;
-      }
-
-      final int size = buffer.getInt();
-      final Set<String> parents = new HashSet<>(size);
-      for (int i = 0; i < size; i++) {
-        final int bytesLength = buffer.getInt();
-        final byte[] bytes = new byte[bytesLength];
-        buffer.get(bytes);
-        final String parent = new String(bytes, DEFAULT_CHARSET);
-        parents.add(parent);
-      }
-
-      return parents;
-    }
-
-    @Override
-    public void close() {
-      this.byteBufferDeserializer.close();
-    }
-
+    final ParentsSerde parentsSerde = new ParentsSerde();
+    return SimpleSerdes.create(parentsSerde, parentsSerde);
+    // return SimpleSerdes.create(new ParentsSerde()); //TODO
   }
 
 }
--- src/main/java/titan/ccp/history/streamprocessing/RecordAggregator.java
+++ src/main/java/titan/ccp/history/streamprocessing/RecordAggregator.java
@@ -3,8 +3,14 @@ package titan.ccp.history.streamprocessing;
 import titan.ccp.models.records.ActivePowerRecord;
 import titan.ccp.models.records.AggregatedActivePowerRecord;
 
+/**
+ * Updates an {@link AggregatedActivePowerRecord} by a new {@link ActivePowerRecord}.
+ */
 public class RecordAggregator {
 
+  /**
+   * Adds an {@link ActivePowerRecord} to an {@link AggregatedActivePowerRecord}.
+   */
   public AggregatedActivePowerRecord add(final String identifier,
       final ActivePowerRecord record, final AggregatedActivePowerRecord aggregated) {
     final long count = (aggregated == null ? 0 : aggregated.getCount()) + 1;
@@ -14,6 +20,9 @@ public class RecordAggregator {
         0.0, 0.0, count, sum, sum / count);
   }
 
+  /**
+   * Substracts an {@link ActivePowerRecord} to an {@link AggregatedActivePowerRecord}.
+   */
   public AggregatedActivePowerRecord substract(final String identifier,
       final ActivePowerRecord record, final AggregatedActivePowerRecord aggregated) {
     final long count = aggregated.getCount() - 1;
