fcc9923751db0615fe2cf1937fa1f012007992aa
Sören Henning <post@soeren-henning.de>
2019-11-18 14:11:19 +0100

    Use refactored PropertiesBuilder
    (to be outsourced in commons project)

--- src/main/java/titan/ccp/history/streamprocessing/PropertiesBuilder.java
+++ src/main/java/titan/ccp/history/streamprocessing/PropertiesBuilder.java
@@ -2,41 +2,49 @@ package titan.ccp.history.streamprocessing;
 
 import java.util.Properties;
 import java.util.function.Predicate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 /**
- * Helper class for constructing and logging Kafka Stream {@link Properties}.
+ * Interface for a helper class for constructing and logging Kafka Stream {@link Properties}.
  */
-public class PropertiesBuilder {
-
-  private static final Logger LOGGER = LoggerFactory.getLogger(KafkaStreamsBuilder.class);
-
-  private final Properties properties = new Properties();
+public interface PropertiesBuilder {
 
   /**
    * Set the provided configuration key to the provided value.
    */
-  public <T> PropertiesBuilder set(final String configKey, final T value) {
-    this.properties.put(configKey, value);
-    LOGGER.info("Set Kafka Streams configuration parameter '{}' to '{}'.", configKey, value);
-    return this;
-  }
+  <T> PropertiesBuilder set(String configKey, T value);
 
   /**
    * Set the provided configuration key to the provided value if a given condition is evaluated to
    * true.
    */
-  public <T> PropertiesBuilder set(final String configKey, final T value,
-      final Predicate<T> condition) {
-    if (condition.test(value)) {
-      this.set(configKey, value);
-    }
-    return this;
+  <T> PropertiesBuilder set(String configKey, T value, Predicate<T> condition);
+
+  /**
+   * Build a {@link Properties} object with the option being set before.
+   */
+  Properties build();
+
+  /**
+   * Interface representing an Kafka Stream {@link Properties} builder without the application id
+   * yet being set.
+   */
+  interface WithoutApplicationId {
+
+    /**
+     * Continue building Kafka Stream properties by specifying an application id. From now on, all
+     * configuration properties can be set directly.
+     */
+    PropertiesBuilder applicationId(String applicationId);
+
   }
 
-  public Properties build() {
-    return this.properties;
+  /**
+   * Start building Kafka Stream properties by specifying a bootstrap server. Next, an application
+   * id has to be specified.
+   */
+  static PropertiesBuilder.WithoutApplicationId bootstrapServers(final String bootstrapServers) {
+    return PropertiesBuilderImpl.bootstrapServers(bootstrapServers);
   }
 
 }
